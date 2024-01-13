96ed2cb636935c343198262dec497e5e509093a6
Prakash <saiprakash8376@gmail.com>
2024-01-13 22:37:49 +0530

    Gradle to Maven Convert

--- src/main/java/com/example/trade/model/Box.java
+++ src/main/java/com/example/trade/model/Box.java
@@ -4,9 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 @Entity
 @Data
@@ -21,32 +18,6 @@ public class Box {
     private BigDecimal boxWeight;
 
     private Long customerId;
-
-//    @Transient
-//    private Map<String, String> changes = new HashMap<>();
-//
-//    @PrePersist
-//    @PreUpdate
-//    private void prePersistOrUpdate() {
-//        // This method will be called before the entity is persisted or updated
-//
-//        // You can compare the current state with the previous state and record changes
-//        if (this.id != null) {
-//            Box previousState = boxRepository.findById(this.id).orElse(null);
-//
-//            if (previousState != null) {
-//                compareAndRecordChange("boxName", previousState.getBoxName(), this.boxName);
-//                compareAndRecordChange("boxWeight", previousState.getBoxWeight().toString(), this.boxWeight.toString());
-//                compareAndRecordChange("customerId", previousState.getCustomerId().toString(), this.customerId.toString());
-//            }
-//        }
-//    }
-//
-//    private void compareAndRecordChange(String fieldName, String oldValue, String newValue) {
-//        if (!Objects.equals(oldValue, newValue)) {
-//            changes.put(fieldName, "From '" + oldValue + "' to '" + newValue + "'");
-//        }
-//    }
 
 }
 
